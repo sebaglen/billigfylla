d1ce86675c982879783704a86c6aa84d992665b5
Sebastian Aglen Danielsen <sd@udp.no>
2021-02-13 01:45:07 +0100

    add images

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images:  {
+        domains: ['bilder.vinmonopolet.no']
+    }
+}
--- src/components/AlkoCard.tsx
+++ src/components/AlkoCard.tsx
@@ -1,4 +1,5 @@
-import { Text, Box, Heading, Spinner, Button } from "@chakra-ui/react"
+import { Text, Box, Heading, Spinner, Button, Stack } from "@chakra-ui/react"
+import Image from 'next/image';
 import React from "react";
 
 export const AlkoCard = ({ alko, isMain = false }: { alko?: Alko, isMain?: boolean }) => {
@@ -9,10 +10,17 @@ export const AlkoCard = ({ alko, isMain = false }: { alko?: Alko, isMain?: boole
         </Box>
     }
 
-    return <Box borderWidth="1px" borderRadius="lg" p="6" boxShadow={isMain ? "lg" : "sm"}>
-        <Heading size={isMain ? "lg" : "md"}>{alko.name}</Heading>
-        <Text>NOK pr alkoliter: {alko.alkPerNOK.toFixed(2)},-</Text>
-        <Button as="a" href={`http://www.vinmonopolet.no/vareutvalg/varedetaljer/sku-${alko.productId}`}>Vis på Vinmonopolet</Button>
+    return <Box borderWidth="1px" borderRadius="lg" p="3" boxShadow={isMain ? "lg" : "sm"}>
+        <Stack direction="row" alignItems={'center'}>
+            <Box padding='0 20px 0 20px'>
+                <Image src={`https://bilder.vinmonopolet.no/cache/300x300-0/${alko.productId}-1.jpg`} objectFit={'contain'} width={isMain ? 50 : 35} height={isMain ? 150 : 75}/>
+            </Box>
+            <Box>
+                <Heading size={isMain ? "lg" : "md"}>{alko.name}</Heading>
+                <Text>NOK pr alkoliter: {alko.alkPerNOK.toFixed(2)},-</Text>
+                <Button as="a" href={`http://www.vinmonopolet.no/vareutvalg/varedetaljer/sku-${alko.productId}`}>Vis på Vinmonopolet</Button>
+            </Box>
+        </Stack>
     </Box>
 };
 
