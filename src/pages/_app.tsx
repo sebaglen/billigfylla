c3ed1e5508c5368d32ee62af3c8a76ffba96cfee
mortenhillbom <mortenhillbom@gmail.com>
2021-03-07 22:49:37 +0100

    Add getInitialProps to avoid automatic static optimization which messes up initial query params

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import '../../global-styles.css';
 
-import { AppProps } from 'next/app';
+import App, { AppContext, AppProps } from 'next/app';
 import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -11,5 +11,12 @@ function MyApp({ Component, pageProps }: AppProps) {
     </ChakraProvider>
   );
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+
+  return { ...appProps };
+};
 
 export default MyApp;
